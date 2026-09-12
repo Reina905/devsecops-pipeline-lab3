0a720405f57952f255e1c5a675e52f24e4b43c5e
Jose Milan <20245436@esen.edu.sv>
2026-09-12 09:06:49 -0600

    main tf modificado

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,6 +8,14 @@ terraform {
   }
 }
 
+backend "s3" {
+    bucket         = "devsecops-lab-tfstate-2026"
+    key            = "static-site/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
 provider "aws" {
   region = "us-east-1"
 }
